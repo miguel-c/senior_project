0ebe61696f2f80221c1165ceaa9896fe3d72b978
Lauren Herschberg <lherschb@villanova.edu>
2016-11-09 13:34:18 -0500

    Team sign up

--- hello-world/src/TeamSignUpPage.jsx
+++ hello-world/src/TeamSignUpPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Button from 'react-button'
+import CustomDrop from './CustomDropDown'
+import Header from './Header'
 
 var divstyle = {
 width: 875,
@@ -13,13 +15,15 @@ textAlign: 'center'
 export default React.createClass({
   handleSubmit: function(e){
       console.log("Submit");
+      location.href = "#/SuccessfulTeamSignUp";
   },
-
 
   render() {
     return(
       <div>
+        <Header/>
         <div style={HeadStyle}><h1>Team Sign Up Page</h1></div>
+
         <div style={HeadStyle}><h3>Fill out the form below to create a new team.</h3></div>
         <div style={divstyle}>
           <label>Team Name: </label><input type="text"/>
@@ -30,14 +34,11 @@ export default React.createClass({
           <br/>
           <label>Captain Name: </label><input type="text"/>
           <br/>
-          <label>Player1: </label><input type="text"/>
-          <br/>
-          <label>Player2: </label><input type="text"/>
-          <br/>
-          <label>Player3: </label><input type="text"/>
-          <br/>
-          <label>Player4: </label><input type="text"/>
-          <br/>
+
+          <CustomDrop></CustomDrop>
+          <CustomDrop></CustomDrop>
+          <CustomDrop></CustomDrop>
+
           <Button onClick={this.handleSubmit} activeStyle={{position:'relative', top: 1}}>Submit</Button>
         </div>
       </div>
